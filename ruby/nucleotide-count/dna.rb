f180ff75ab793fffe7c818d89c2d1ce522f28f90
Han Loong Liauw <hanloongliauw@gmail.com>
2013-12-14 23:20:59 +1100

    more refactoring

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,34 +1,44 @@
 class DNA
 
   def initialize sequence
-    @sequence = sequence.chars
-    @sequence.each do |c|
-      raise ArgumentError unless base_list.has_key?(c)
-    end
+    @sequence = Sequence.new sequence
+    raise ArgumentError and return nil unless @sequence.valid?
   end
 
   def count search
     raise ArgumentError and return false unless valid_search?(search)
-    @sequence.count { |c| search == c }
+    @sequence.count(search)
   end
 
   def nucleotide_counts
-    base_list.map { |key, value|
-      {key => @sequence.select{ |c| key == c}.size }
+    Sequence.base_list.map { |key, value|
+      {key => @sequence.count(key) }
     }.reduce(:merge)
   end
 
   private
 
   def valid_search?(search)
-    search.empty? || full_list.has_key?(search)
+    search.empty? || Sequence.full_list.has_key?(search)
   end
 
-  def base_list
+end
+
+class Sequence < String
+
+  def count(search)
+    chars.count { |c| search == c }
+  end
+
+  def valid?
+    gsub(Regexp.new("[#{self.class.base_list.keys.join}]"), '').empty?
+  end
+
+  def self.base_list
     {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0}
   end
 
-  def full_list
+  def self.full_list
     {'U' => 0}.merge(base_list)
   end
 end
